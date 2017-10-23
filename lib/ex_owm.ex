99047f17eb278996bc2104599b3c3f39d4f0ecdd
Rafal Kociszewski <rafal.kociszewski@gmail.com>
2017-10-23 16:06:06 +0200

    update api key, add primitive api call for test

--- lib/ex_owm.ex
+++ lib/ex_owm.ex
@@ -4,15 +4,46 @@ defmodule ExOwm do
   """
 
   @doc """
-  Hello world.
+  Gets temperature of the given city by calling api.openweathermap.org
+
+  ## Example API calls by city name
+  api.openweathermap.org/data/2.5/weather?q={city name}&APPID={APIKEY}
+  api.openweathermap.org/data/2.5/weather?q={city name},{country code}&units=metric&APPID={APIKEY}
+
+  ## Example API calls by city ID
+  api.openweathermap.org/data/2.5/weather?id=2172797&APPID={APIKEY}
+
+  ## Full parameter list: http://openweathermap.org/current#format
 
   ## Examples
 
-      iex> ExOwm.hello
+      iex> WeatherEx.temperature_of("Warsaw")
       :world
 
   """
-  def hello do
-    :world
+  def get_weather(location) do
+    location
+    |> send_request()
+    |> parse_request()
+  end
+
+  defp send_request(location) do
+    api_url = "api.openweathermap.org/data/2.5/weather?q=#{location}&units=metric&APPID=#{Application.get_env(:ex_owm, :api_key)}"
+    case HTTPoison.get(api_url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: json_body}} ->
+        {:ok, json_body}
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        {:ok, :not_found}
+      {:ok, %HTTPoison.Response{status_code: 400}} ->
+        {:ok, :empty_location}  
+    end
+  end
+
+  defp parse_request(location) do
+    case location do
+      {:ok, json_body} ->
+        {:ok, weather_map} = Poison.decode(json_body)
+        weather_map
+    end
   end
 end
