8bbb1e3e92e1ab14e029bcac964557005be08d22
Kociamber <rafal.kociszewski@mineralmidrange.com>
2017-12-17 20:02:29 +0100

    add basic tests for get_current_weather_data/1 and get_current_weather_data/2

--- test/ex_owm_test.exs
+++ test/ex_owm_test.exs
@@ -2,12 +2,158 @@ defmodule ExOwmTest do
   use ExUnit.Case
   doctest ExOwm
 
-  test ": can get temperature for a given location" do
+  test ": can get weather data with get_current_weather_data/1 by city name" do
     # given
-    location = "Warsaw"
+    city = %{city: "Warsaw"}
     # when
-    result = ExOwm.get_weather(location)
+    result = ExOwm.get_current_weather_data([city])
     # then
-    assert is_map(result)
+    # check whether a list of maps is returned
+    assert is_list(result)
+    assert result != []
+    map = List.first(result)
+    assert is_map(map)
+    # check wether map has specific keys to confirm that request was successful
+    assert Map.get(map, "name") == "Warsaw"
+  end
+
+  test ": can get weather data with get_current_weather_data/1 by city name and country code" do
+    # given
+    city = %{city: "Warsaw", countr_code: "pl"}
+    # when
+    result = ExOwm.get_current_weather_data([city])
+    # then
+    # check whether a list of maps is returned
+    assert is_list(result)
+    assert result != []
+    map = List.first(result)
+    assert is_map(map)
+    # check wether map has specific keys to confirm that request was successful
+    assert Map.get(map, "name") == "Warsaw"
+  end
+
+  test ": can get weather data with get_current_weather_data/1 by city id" do
+    # given
+    city = %{id: 2759794}
+    # when
+    result = ExOwm.get_current_weather_data([city])
+    # then
+    # check whether a list of maps is returned
+    assert is_list(result)
+    assert result != []
+    map = List.first(result)
+    assert is_map(map)
+    # check wether map has specific keys to confirm that request was successful
+    assert Map.get(map, "name") == "Amsterdam"
+  end
+
+  test ": can get weather data with get_current_weather_data/1 by latitude and longitude" do
+    # given
+    city = %{lat: 52.374031, lon: 4.88969}
+    # when
+    result = ExOwm.get_current_weather_data([city])
+    # then
+    # check whether a list of maps is returned
+    assert is_list(result)
+    assert result != []
+    map = List.first(result)
+    assert is_map(map)
+    # check wether map has specific keys to confirm that request was successful
+    assert Map.get(map, "name") == "Amsterdam"
+  end
+
+  test ": can get weather data with get_current_weather_data/1 by zip and country code" do
+    # given
+    city = %{zip: "94040", country_code: "us"}
+    # when
+    result = ExOwm.get_current_weather_data([city])
+    # then
+    # check whether a list of maps is returned
+    assert is_list(result)
+    assert result != []
+    map = List.first(result)
+    assert is_map(map)
+    # check wether map has specific keys to confirm that request was successful
+    assert Map.get(map, "name") == "Mountain View"
+  end
+
+  test ": can get weather data with get_current_weather_data/1 by city name with options" do
+    # given
+    city = %{city: "Warsaw"}
+    options = [units: :metric, lang: :pl]
+    # when
+    result = ExOwm.get_current_weather_data([city], options)
+    # then
+    # check whether a list of maps is returned
+    assert is_list(result)
+    assert result != []
+    map = List.first(result)
+    assert is_map(map)
+    # check wether map has specific keys to confirm that request was successful
+    assert Map.get(map, "name") == "Warsaw"
+  end
+
+  test ": can get weather data with get_current_weather_data/1 by city name and country code with options" do
+    # given
+    city = %{city: "Warsaw", countr_code: "pl"}
+    options = [units: :metric, lang: :pl]
+    # when
+    result = ExOwm.get_current_weather_data([city], options)
+    # then
+    # check whether a list of maps is returned
+    assert is_list(result)
+    assert result != []
+    map = List.first(result)
+    assert is_map(map)
+    # check wether map has specific keys to confirm that request was successful
+    assert Map.get(map, "name") == "Warsaw"
+  end
+
+  test ": can get weather data with get_current_weather_data/1 by city id with options" do
+    # given
+    city = %{id: 2759794}
+    options = [units: :metric, lang: :pl]
+    # when
+    result = ExOwm.get_current_weather_data([city], options)
+    # then
+    # check whether a list of maps is returned
+    assert is_list(result)
+    assert result != []
+    map = List.first(result)
+    assert is_map(map)
+    # check wether map has specific keys to confirm that request was successful
+    assert Map.get(map, "name") == "Amsterdam"
+  end
+
+  test ": can get weather data with get_current_weather_data/1 by latitude and longitude with options" do
+    # given
+    city = %{lat: 52.374031, lon: 4.88969}
+    options = [units: :metric, lang: :pl]
+    # when
+    result = ExOwm.get_current_weather_data([city], options)
+    # then
+    # check whether a list of maps is returned
+    assert is_list(result)
+    assert result != []
+    map = List.first(result)
+    assert is_map(map)
+    # check wether map has specific keys to confirm that request was successful
+    assert Map.get(map, "name") == "Amsterdam"
+  end
+
+  test ": can get weather data with get_current_weather_data/1 by zip and country code with options" do
+    # given
+    city = %{zip: "94040", country_code: "us"}
+    options = [units: :metric, lang: :pl]
+    # when
+    result = ExOwm.get_current_weather_data([city], options)
+    # then
+    # check whether a list of maps is returned
+    assert is_list(result)
+    assert result != []
+    map = List.first(result)
+    assert is_map(map)
+    # check wether map has specific keys to confirm that request was successful
+    assert Map.get(map, "name") == "Mountain View"
   end
 end
