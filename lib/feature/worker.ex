1696ace88806cd86a66b3a3f32a49aa785057ced
Rafal Kociszewski <rafal.kociszewski@gmail.com>
2017-12-18 13:26:17 +0100

    add primitiva caching

--- lib/feature/worker.ex
+++ lib/feature/worker.ex
@@ -1,7 +1,21 @@
 defmodule ExOwm.Feature.Worker do
   alias ExOwm.Feature.ApiCaller
-  
+
+  # @todo:
+  # 1. add timestamp interval
+  # 2. Consider using generational cache, like: https://github.com/cabol/nebulex
+
   def run(location, opts) do
-    ApiCaller.send_and_parse_request(location, opts)
+    # Check wether the request has been been already cached.
+    case :ets.lookup(:exowm_cache, location) do
+      [] ->
+        # No location in cache - fetch API call result and cache it.
+        result = ApiCaller.send_and_parse_request(location, opts)
+        :ets.insert(:exowm_cache, {location ,result, DateTime.utc_now()})
+        result
+      [result] ->
+        # Request already processed and chached - return it. 
+        Kernel.elem(result, 1)
+    end
   end
 end
