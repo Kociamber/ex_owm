7e6254fe54d257f980ea1d981aeb2cfa20fb0c72
Rafal Kociszewski <rafal.kociszewski@gmail.com>
2017-12-27 11:45:28 +0100

    re-factor coordinator and remove ETS

--- lib/ex_owm/coordinator.ex
+++ lib/ex_owm/coordinator.ex
@@ -11,13 +11,12 @@ defmodule ExOwm.Coordinator do
     GenServer.call(:exowm_coordinator, {:get_state})
   end
 
-  def start_workers(request_type, locations, opts) do
-    GenServer.call(:exowm_coordinator, {:start_workers, request_type, locations, opts})
+  def start_workers(:get_current_weather, locations, opts) do
+    GenServer.call(:exowm_coordinator, {:start_workers, locations, opts})
   end
 
   ## Server implementation
   def init(_) do
-    :ets.new(:exowm_cache, [:named_table, :public, read_concurrency: true, write_concurrency: true])
     {:ok, %{}}
   end
 
@@ -25,12 +24,8 @@ defmodule ExOwm.Coordinator do
     {:reply, state, state}
   end
 
-  def handle_call({:start_workers, request_type, locations, opts}, _from, _state) do
-    worker_tasks =
-      case request_type do
-        :get_current_weather ->
-          Enum.map(locations, fn(location) -> Task.async(Worker, :get_current_weather, [location, opts]) end)
-      end
+  def handle_call({:start_workers, locations, opts}, _from, _state) do
+    worker_tasks = Enum.map(locations, fn(location) -> Task.async(Worker, :get_current_weather, [location, opts]) end)
     results = Enum.map(worker_tasks, fn(task) -> Task.await(task) end)
     {:reply, results, results}
   end
