877e1f800433c3abe5097bf478334a7483b44e9d
Rafal Kociszewski <rafal.kociszewski@gmail.com>
2017-12-28 13:56:09 +0100

    re-factor request string module

--- lib/ex_owm/request_string.ex
+++ lib/ex_owm/request_string.ex
@@ -8,31 +8,34 @@ defmodule ExOwm.RequestString do
     |> add_prefix_substring()
     |> add_location_substring()
     |> add_search_accuracy_substring()
+    |> add_format_substring()
     |> add_units_substring()
     |> add_language_substring()
     |> add_api_key_substring()
   end
 
-  # Start building querys string.
+  # Current weather call.
   defp add_prefix_substring({:get_current_weather, location, opts}),
     do: {"api.openweathermap.org/data/2.5/weather", location, opts}
-
+  # Five day forecast call.
   defp add_prefix_substring({:get_five_day_forecast, location, opts}),
     do: {"api.openweathermap.org/data/2.5/forecast", location, opts}
 
-  # Add location type to query string.
+  #api.openweathermap.org/data/2.5/forecast/daily
+
+  # Call by city name.
   defp add_location_substring({string, %{city: city}, opts}),
     do: {string <> "?q=#{city}", opts}
-
+  # Call by city name and ISO 3166 country code.
   defp add_location_substring({string, %{city: city, country_code: country_code}, opts}),
     do: {string <> "?q=#{city},#{country_code}", opts}
-
+  # Call by city id.
   defp add_location_substring({string, %{id: id}, opts}),
     do: {string <> "?id=#{id}", opts}
-
+  # Call by geo coordinates.
   defp add_location_substring({string, %{lat: lat, lon: lon}, opts}),
     do: {string <> "?lat=#{lat}&lon=#{lon}", opts}
-
+  # Call by zip and ISO 3166 country code.
   defp add_location_substring({string, %{zip: zip, country_code: country_code}, opts}),
     do: {string <> "?zip=#{zip},#{country_code}", opts}
 
@@ -63,6 +66,9 @@ defmodule ExOwm.RequestString do
     end
   end
 
+  # Add language code parameter. API call will return data with description field
+  # in selected language. List of available languages can be found here:
+  # http://openweathermap.org/forecast16#multi
   defp add_language_substring({string, opts}) do
     case Keyword.has_key?(opts, :lang) do
       true ->
@@ -75,5 +81,6 @@ defmodule ExOwm.RequestString do
     end
   end
 
+  # Add API key.
   defp add_api_key_substring(string), do: string <> "&APPID=#{Application.get_env(:ex_owm, :api_key)}"
 end
