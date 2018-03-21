6ced97faf0e4b3c94fda49e45504172a89ba58f6
Kociamber <rafal.kociszewski@gmail.com>
2018-03-21 12:46:21 +0100

    remove comments

--- lib/application.ex
+++ lib/application.ex
@@ -1,8 +1,6 @@
 defmodule ExOwm.Application do
   require Logger
   use Application
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
 
   def start(_type, _args) do
     import Supervisor.Spec
