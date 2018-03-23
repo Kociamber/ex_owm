f9153508e37eeeefa866f2c32509311e8deea528
Kociamber <rafal.kociszewski@gmail.com>
2018-03-23 11:51:46 +0100

    add mix meta data

--- mix.exs
+++ mix.exs
@@ -1,17 +1,27 @@
 defmodule ExOwm.Mixfile do
   use Mix.Project
+  @github_url "https://github.com/Kociamber/ex_owm"
 
   def project do
     [
       app: :ex_owm,
-      version: "0.1.0",
+      name: "ExOwm",
+      version: "1.0.0",
+      description: "OpenWeatherMap API Elixir client.",
+      source_url: @github_url,
+      homepage_url: @github_url,
+      package: [
+        maintainers: ["Rafał Kociszewski"],
+        licenses: ["MIT"],
+        links: %{"GitHub" => @github_url}
+      ],
       elixir: "~> 1.6",
       start_permanent: Mix.env == :prod,
       deps: deps(),
       docs: [
-      main: "readme",
-      extras: ["README.md", "CHANGELOG.md"]
-]
+        main: "readme",
+        extras: ["README.md", "CHANGELOG.md"]
+      ]
     ]
   end
 
